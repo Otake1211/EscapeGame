c69b232aa5a3ab65a3e3306e12f6cbb4243adab4
Otake <taketo1211@outlook.jo>
2020-11-29 02:37:55 +0900

    make 3 activity main, setting and north

--- app/src/main/java/com/example/escapegame/MainActivity.java
+++ app/src/main/java/com/example/escapegame/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.escapegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSetting(View view) {
+        switch (view.getId()) {
+            case R.id.Setting:
+            Intent intent = new  Intent(this, Setting.class);
+        startActivity(intent);break;
+        }
+    }
+
+    public void onStart(View view) {
+        switch (view.getId()) {
+            case R.id.Start:
+                Intent intent = new  Intent(this, north.class);
+                startActivity(intent);break;
+        }
     }
 }
