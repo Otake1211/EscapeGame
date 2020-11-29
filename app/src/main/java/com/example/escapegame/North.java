93870f3f4d62639ac0ee40f3d5be36b98c0cf198
Otake <taketo1211@outlook.jo>
2020-11-29 22:08:19 +0900

    modify a little.

--- app/src/main/java/com/example/escapegame/North.java
+++ app/src/main/java/com/example/escapegame/North.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebStorage;
 
 public class North extends AppCompatActivity {
 
