2b87c3672ee3ea400bef0b58ffa9aeb865e9ec11
toluyu <yuk9_mail.address@icloud.com>
2020-12-01 11:56:33 +0900

    touch control in MainActivity.java

--- app/src/main/java/com/example/escapegame/MainActivity.java
+++ app/src/main/java/com/example/escapegame/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.escapegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,18 +20,51 @@ public class MainActivity extends AppCompatActivity {
     public void onSetting(View view) {
         switch (view.getId()) {
             case R.id.Setting:
-            Intent intent = new  Intent(this, Setting.class);
-        startActivity(intent);
-        break;
+                Intent intent = new Intent(this, Setting.class);
+                startActivity(intent);
+                break;
         }
     }
 
     public void onNorth(View view) {
         switch (view.getId()) {
             case R.id.North:
-                Intent intent = new  Intent(this, North.class);
+                Intent intent = new Intent(this, North.class);
                 startActivity(intent);
+
+
                 break;
+
+
         }
     }
+
+
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                float xplace = motionEvent.getX();
+                float yplace = motionEvent.getY();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(xplace+","+yplace)
+                        .setPositiveButton("#", null);
+                builder.show();
+                break;
+            case MotionEvent.ACTION_UP:
+                // something to do
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // something to do
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                // something to do
+                break;
+        }
+
+        return false;
+    }
+
+
 }
