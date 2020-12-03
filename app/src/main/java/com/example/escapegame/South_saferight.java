c6e28d19b39b2863433c2c9f31bac594ff640c4b
Otake <taketo1211@outlook.jo>
2020-12-04 01:14:45 +0900

    some activity add.

---
+++ app/src/main/java/com/example/escapegame/South_saferight.java
@@ -0,0 +1,14 @@
+package com.example.escapegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class South_saferight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_south_saferight);
+    }
+}
