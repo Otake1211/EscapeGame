48d70b4fb31412c8b5cdd6d1168890803b39c2d9
toluyu <yuk9_mail.address@icloud.com>
2020-12-04 01:09:06 +0900

    create new activities

---
+++ app/src/main/java/com/example/escapegame/East_wave.java
@@ -0,0 +1,14 @@
+package com.example.escapegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class East_wave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_east_wave);
+    }
+}
