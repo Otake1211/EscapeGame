356de44e227f0511473749defef9d197a829c50c
toluyu <yuk9_mail.address@icloud.com>
2020-12-06 01:15:44 +0900

    south key get

--- app/src/main/java/com/example/escapegame/West_drawerright.java
+++ app/src/main/java/com/example/escapegame/West_drawerright.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class West_drawerright extends AppCompatActivity {
+
+    int screenWidth;
+    int screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,4 +32,32 @@ public class West_drawerright extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        int xplace = (int) (motionEvent.getX() * 1000 / screenWidth);
+        int yplace = (int) (motionEvent.getY() * 2000 / screenHeight);
+
+        switch (motionEvent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN: //タップしたとき
+
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                // something to do
+                break;
+        }
+        return false;
+    }
+}
 }
