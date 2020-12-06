1578c19c2018d87aed2af46d28fb1dddf72e4a39
toluyu <yuk9_mail.address@icloud.com>
2020-12-06 13:08:28 +0900

    little change

--- app/src/main/java/com/example/escapegame/South_saferight.java
+++ app/src/main/java/com/example/escapegame/South_saferight.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class South_saferight extends AppCompatActivity {
 
@@ -39,5 +41,50 @@ public class South_saferight extends AppCompatActivity {
         Intent intent = new Intent(this, South.class);
         startActivity(intent);
         finish();
+    }
+
+
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        int xplace = (int) (motionEvent.getX() * 1000 / screenWidth);
+        int yplace = (int) (motionEvent.getY() * 2000 / screenHeight);
+
+        switch (motionEvent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN: //タップしたとき
+
+                ImageView backimage = ((ImageView) findViewById(R.id.backimage));
+                if (touchcount == 0) {
+                    // 開く操作
+                    if (true) {
+                        //アイテムある時の画像
+                        backimage.setImageResource(R.drawable.south_rockerrighton);
+                    } else {
+                        //アイテムない時の画像
+                        backimage.setImageResource(R.drawable.south_rockerrightoff);
+                    }
+                    touchcount = 2;
+                } else {
+                    //開いた後の操作
+                    if (850 < xplace && xplace < 940 && 540 < yplace && yplace < 646) {
+                        //アイテムとった判定
+                    }
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                // something to do
+                break;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/escapegame/West_drawerleft.java
+++ app/src/main/java/com/example/escapegame/West_drawerleft.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class West_drawerleft extends AppCompatActivity {
 
@@ -31,5 +33,48 @@ public class West_drawerleft extends AppCompatActivity {
         Intent intent = new Intent(this, West.class);
         startActivity(intent);
         finish();
+    }
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        int xplace = (int) (motionEvent.getX() * 1000 / screenWidth);
+        int yplace = (int) (motionEvent.getY() * 2000 / screenHeight);
+
+        switch (motionEvent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN: //タップしたとき
+
+                ImageView backimage = ((ImageView) findViewById(R.id.backimage));
+                if (touchcount == 0) {
+                    // 開く操作
+                    if (true) {
+                        //アイテムある時の画像
+                        backimage.setImageResource(R.drawable.south_rockerrighton);
+                    } else {
+                        //アイテムない時の画像
+                        backimage.setImageResource(R.drawable.south_rockerrightoff);
+                    }
+                    touchcount = 2;
+                } else {
+                    //開いた後の操作
+                    if (850 < xplace && xplace < 940 && 540 < yplace && yplace < 646) {
+                        //アイテムとった判定
+                    }
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                // something to do
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                // something to do
+                break;
+        }
+        return false;
     }
 }
